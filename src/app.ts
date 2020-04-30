fe7f346e5ab744eb2fa778a3860d65871be1ff3d
unknown <arandarenzogabriel@gmail.com>
2020-04-30 13:57:00 -0300

    version 3 - post

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { response } from 'express';
 import path = require("path");
 
 const app = express();
@@ -9,4 +9,9 @@ app.get('/',(req,res) => {
     res.sendFile('index.html', {root: path.join(__dirname, './')});
 });
 
+app.post('/',function(req,res,next){
+    res.redirect('/');
+});
+
 app.listen(5000, () => console.log('Server running'));
+
