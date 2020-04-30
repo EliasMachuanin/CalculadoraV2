1e511c29b41d965d750d3e60ba22bcbd6ee74476
EliasMachuanin <eliasmachuanin@gmail.com>
2020-04-30 20:01:05 -0300

    Calculadora funcionano.

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,27 @@
-import express from 'express';
+import express, { response } from 'express';
 import path = require("path");
+import Calculadora from './Calculadora';
+import Parser from './Parser';
+import Contexto from './Contexto';
+var body_parser = require('body-parser');
 
 const app = express();
 
-app.use('/', express.static(__dirname + '/'))
+let c = new Calculadora()
+let p = new Parser()
 
-app.get('/',(req,res) => {
-    res.sendFile('index.html', {root: path.join(__dirname, './')});
+app.use(body_parser.urlencoded({extended:true}));
+
+app.use('/', express.static('src'))
+
+app.post('/',function(req,res,next){
+    
+    var entrada = req.body.textbox
+    let newC = new Contexto(entrada)
+    c.procesarNuevoElemento(p.evaluate(newC))
+    console.log(c.resultado)
+    console.log(entrada)
+    res.redirect('/');
 });
 
 app.listen(5000, () => console.log('Server running'));
