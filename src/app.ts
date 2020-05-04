631b1061c14c0b56fc82638c4681b7374dbc53b1
EliasMachuanin <eliasmachuanin@gmail.com>
2020-05-04 17:59:09 -0300

    Cambios en el SE, tiempo real.

--- src/app.ts
+++ src/app.ts
@@ -6,53 +6,72 @@ import OperacionResta from "./OperacionResta";
 import OperacionProducto from "./OperacionProducto";
 import OperacionDivision from "./OperacionDivision";
 
-var express = require ('express');
-var response = require ('express');
-var path = require("path");
+const express = require('express'),
+  es6Renderer = require('express-es6-template-engine'),
+  app = express();
+  
+app.engine('html', es6Renderer);
+app.set('views', 'views');
+app.set('view engine', 'html');
 var body_parser = require('body-parser');
-
-const app = express();
-
 let c = new Calculadora();
 let p = new Parser();
-
 app.use(body_parser.urlencoded({extended:true}));
 
-app.use('/', express.static('src'));
+app.use('/', express.static('img'));
 
-app.set('view engine','ejs');
+app.get('/', function(req, res) {
+    if(c.resultado!=null&&c.operacion==null){
+        res.render('index', {locals: {salida: c.resultado}});
+    }
+    else if(c.operacion!=null){
+        if(c.operacion instanceof OperacionSuma){
+            res.render('index',{locals:{salida: c.resultado.toString().concat(" +")}});
+        }
+        else if(c.operacion instanceof OperacionResta){
+            res.render('index',{locals:{salida: c.resultado.toString().concat(" -")}});
+        }
+        else if(c.operacion instanceof OperacionProducto){
+            res.render('index',{locals:{salida: c.resultado.toString().concat(" *")}});
+        }
+        else if(c.operacion instanceof OperacionDivision){
+            res.render('index',{locals:{salida: c.resultado.toString().concat(" /")}});
+        }
+        
+    }
+    else{
+        res.render('index', {locals: {salida: 0}});
+    }
+    
+  });
 
-app.get('/',function(req,res){
-    res.sendFile(__dirname + '/index.html')
-})
+
 
 app.post('/',function(req,res){
     var entrada = req.body.textbox;
     let newC = new Contexto(entrada);
     c.procesarNuevoElemento(p.evaluate(newC));
     if(c.resultado!=null&&c.operacion==null){
-        res.render('resultado',{salida: c.resultado});
-        console.log(c.resultado)
+        res.render('index', {locals: {salida: c.resultado}});
     }
     else if(c.operacion!=null){
         if(c.operacion instanceof OperacionSuma){
-            res.render('resultado',{salida: c.resultado.toString().concat(" +")});
+            res.render('index',{locals:{salida: c.resultado.toString().concat(" +")}});
         }
         else if(c.operacion instanceof OperacionResta){
-            res.render('resultado',{salida: c.resultado.toString().concat(" -")});
+            res.render('index',{locals:{salida: c.resultado.toString().concat(" -")}});
         }
         else if(c.operacion instanceof OperacionProducto){
-            res.render('resultado',{salida: c.resultado.toString().concat(" x")});
+            res.render('index',{locals:{salida: c.resultado.toString().concat(" *")}});
         }
         else if(c.operacion instanceof OperacionDivision){
-            res.render('resultado',{salida: c.resultado.toString().concat(" /")});
+            res.render('index',{locals:{salida: c.resultado.toString().concat(" /")}});
         }
         
     }
     else{
-        res.render('resultado',{salida: "SYNTAX ERROR"});
+        res.render('index', {locals: {salida: 0}});
     }
-    
 });
 
 app.listen(5011, () => console.log('Server running'));
